c1d13c7698b037daf7e5222eeffc8d0f49725e69
marcus.pratley <marcus.pratley@ticketmaster.co.uk>
2024-09-13 01:03:21 +0100

    Migrate to cloudflare

---
+++ app/routes/icalfeed.$name.ts
@@ -0,0 +1,32 @@
+import { LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { generateICalFeed } from "~/utils/ical";
+import { fetchGoogleApiData } from "~/utils/googleApi";
+
+// this is a route that returns an ical feed that users can subscribe to
+export async function loader({
+  params,
+  context,
+}: LoaderFunctionArgs) {
+  const name = params.name;
+
+  if (!name) {
+    throw new Response("Name is required", { status: 400 });
+  }
+
+  const data = await fetchGoogleApiData(context, name);
+
+  const icalContent = generateICalFeed(data, name);
+
+  // Set cache control headers
+  const headers = new Headers({
+    "Content-Type": "text/calendar; charset=utf-8",
+    "Cache-Control": "public, max-age=1800", // 30 minutes
+  });
+
+  // Add Last-Modified header if available
+  if (data.lastModified) {
+    headers.set("Last-Modified", new Date(data.lastModified).toUTCString());
+  }
+
+  return new Response(icalContent, { headers });
+}
