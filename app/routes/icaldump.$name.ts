94119b261950acfa69088eb371a4533c8baeb648
Marcus Pratley <marcuspratley@gmail.com>
2024-09-13 09:20:28 +0100

    form cleanup & server module explicitly

--- app/routes/icaldump.$name.ts
+++ app/routes/icaldump.$name.ts
@@ -1,6 +1,6 @@
 import { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { generateICalFeed } from "~/utils/ical";
-import { fetchGoogleApiData } from "~/utils/googleApi";
+import { fetchGoogleApiData } from "~/utils/googleApi.server";
 
 // this is a route that dumps an ical feed as text for debugging
 export async function loader({
--- app/routes/icalfeed.$name.ts
+++ app/routes/icalfeed.$name.ts
@@ -1,6 +1,6 @@
 import { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { generateICalFeed } from "~/utils/ical";
-import { fetchGoogleApiData } from "~/utils/googleApi";
+import { fetchGoogleApiData } from "~/utils/googleApi.server";
 
 // this is a route that returns an ical feed that users can subscribe to
 export async function loader({
